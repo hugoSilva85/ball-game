4827c5e9a5ad7d8a008667e853c09cd10400eb61
Hugo Silva <hugo.pereira.silva85@gmail.com>
2018-03-01 14:00:34 -0300

    Up everything

--- Assets/scripts/ScoreScript.cs
+++ Assets/scripts/ScoreScript.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+﻿	using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -7,6 +7,8 @@ public class ScoreScript : MonoBehaviour {
 
 	public static int scoreValue = 0;
 	Text score;
+	public GameObject gameWinBackGround;
+
 
 	// Use this for initialization
 	void Start () {
@@ -16,11 +18,19 @@ public class ScoreScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		score.text = "Score: " + scoreValue;
+		score.text = "Score: " 	+ scoreValue;
 
-		if (scoreValue >= 50){
-			Debug.Log("Score statico deve ser dinamico referente a soma das bolas existentes na tela");
+
+		int total = GameObject.FindGameObjectsWithTag("blue").Length;
+		int result =  scoreValue * total;
+
+		if (scoreValue >= result){
+			nextLevel();
 		}
 		
+	}
+
+	void nextLevel(){
+		gameWinBackGround.SetActive(true);
 	}
 }
